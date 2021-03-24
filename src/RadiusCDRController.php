7c162ab31586a49ae9cd5b92096561b625657e17
StreitLeak <streitleak@gmail.com>
2021-03-24 16:52:52 +0800

    namespace update

--- src/RadiusCDRController.php
+++ src/RadiusCDRController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Streitleak\Web;
+namespace Streitleak\RadiusWeb;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- src/RadiusCDRServiceProvider.php
+++ src/RadiusCDRServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Streitleak\Web;
+namespace Streitleak\RadiusWeb;
 
 use Illuminate\Support\ServiceProvider;
 
