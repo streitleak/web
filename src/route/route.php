990adae3c832807c4eeb10b219568acce0d8552d
StreitLeak <streitleak@gmail.com>
2021-04-07 10:12:21 +0800

    Update route.php

--- src/route/route.php
+++ src/route/route.php
@@ -1,4 +1,7 @@
 <?php
 Route::get('index',[Streitleak\RadiusWeb\RadiusCDRController::class,'index']);
 Route::match(['get','post'],'cdr',[Streitleak\RadiusWeb\RadiusCDRController::class,'cdr']);
+Route::match(['get','post'],'login',[Streitleak\RadiusWeb\UserController::class,'login']);
+Route::get('logout',[Streitleak\RadiusWeb\UserController::class,'logout']);
+Route::match(['get','post'],'resetpassword',[Streitleak\RadiusWeb\UserController::class,'resetpassword']);
 ?>
