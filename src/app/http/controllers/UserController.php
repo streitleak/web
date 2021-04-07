e91b3b8f00da19a6e3102bf027adc6fff09b003c
StreitLeak <streitleak@gmail.com>
2021-04-07 16:04:27 +0800

    file update

--- src/app/http/controllers/UserController.php
+++ src/app/http/controllers/UserController.php
@@ -5,13 +5,24 @@ namespace Streitleak\RadiusWeb;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     //
-    public function login()
+    public function login(Request $request)
     {
-        return View('radiusweb::auth.login');
+        $credentials = $request->only('email', 'password');
+
+        $login_result = "";
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }        
+
+        return View('radiusweb::auth.login',['login_result' = $login_result ]);
 
     }
 
@@ -24,3 +35,4 @@ class UserController extends Controller
         return View('radiusweb::auth.resetpassword');
     }
 }
+
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,11 +1,13 @@
 @extends('radiusweb::layouts.app')
     
 @section('content')
+<div id=loginform>
 <form method="POST" id=login name=login action="/login">
 <table>
-    <tr><td>ID:</td><td><input name=account id=account type=text length=20></td></tr>
-    <tr><td>ID:</td><td><input name=password id=password type=password length=40></td></tr>
-    <tr><td colspan=2><input type=button value=login> <a href=/resetpassword>Forgotten Password?</a>@csrf</td></tr>
+    <tr><td>ID:</td><td><input name=email id=email type=text length=20></td></tr>
+    <tr><td>Password:</td><td><input name=password id=password type=password length=40></td></tr>
+    <tr><td colspan=2><input type=button value=login> <a href=/resetpassword>Forgotten Password?</a>@csrf<div id=result></div></td></tr>
 </table>
 </form>
+</div>
 @endsection
